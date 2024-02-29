1b8bfbefc024f5d31247afa12feaa290783b6b27
D_Zucho <danny.nico.dev@outlook.com>
2024-02-29 07:34:42 -0800

    add and style register page with form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 import './App.css'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <div className='App'>
-      <h1>header</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+import '../styles/register.scss'
 
 const RegisterPage = () => {
   return (
-    <div>
-      <p>RegisterPage</p>
-    </div>
+    <section className='register'>
+      <div className='register_content'>
+        <form className='register_form'>
+          <h1 className='register_form_title'>REGISTER</h1>
+          <input
+            type='text'
+            placeholder='First Name'
+            name='firstName'
+            required
+          />
+          <input type='text' placeholder='Last Name' name='lastName' required />
+          <input type='email' placeholder='email' name='email' required />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            required
+          />
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='confirmPassword'
+            required
+          />
+          <input
+            type='file'
+            name='profileImage'
+            id='profileImage'
+            accept='image/*'
+            required
+            style={{ display: 'none' }}
+          />
+          <label htmlFor='profileImage'>
+            <img src='/assets/addImage.png' alt='' />
+            Upload Profile Photo
+          </label>
+          <button type='submit'>REGISTER</button>
+        </form>
+        <a href='/login'>Already have an account? Log in Here</a>
+      </div>
+    </section>
   )
 }
 
