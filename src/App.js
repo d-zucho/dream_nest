afd8397a615e3874e60b51be8879e7307816400b
D_Zucho <danny.nico.dev@outlook.com>
2024-02-28 22:59:13 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>header</h1>
+    </div>
+  )
+}
+
+export default App
